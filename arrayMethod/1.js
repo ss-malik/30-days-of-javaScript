0f760c6006fe70a483c9c5304f465908eee090bc
Salman Malik <ssmalik989@gmail.com>
2024-06-26 23:37:36 +0530

    js array methodt

---
+++ arrayMethod/1.js
@@ -0,0 +1,43 @@
+let arrayFriend = [
+  "Salman",
+  "Rohit",
+  "Ajay",
+  "Pankaj",
+  "Anwar",
+  "Umar",
+  "Rishabh",
+  "Rahul",
+];
+
+let friendName = "Anwar Khalid";
+
+console.log(friendName.split(""));
+
+// includes return true or false
+
+// let searchFriend = "Anwar";
+
+// console.log(arrayFriend.includes(searchFriend));
+
+// console.log(arrayFriend.slice(1));
+// console.log(arrayFriend);
+
+// console.log("salman".slice(1));
+
+// loops in array by for loop
+
+// let arrayLength = arrayFriend.length;
+// for (let i = 0; i < arrayLength; i++) {
+//   console.log(array[i]);
+// }
+
+// loops in array by for of loop
+// for (let friend of arrayFriend) {
+//   console.log(friend);
+// }
+
+// loops in array by for in loop
+
+// for (let index in arrayFriend) {
+//   console.log(arrayFriend[index]);
+// }
