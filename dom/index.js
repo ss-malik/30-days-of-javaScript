cd35ca0f8ce8add96e7850c4c372e54c6b90ebb4
Salman Malik <ssmalik989@gmail.com>
2024-06-29 23:41:20 +0530

    dom

---
+++ dom/index.js
@@ -0,0 +1,20 @@
+// const startButton = document.getElementById("start");
+
+// console.log(startButton);
+
+// const stopButton = document.getElementById("stop");
+
+// console.log(stopButton);
+
+// const startButton = document.getElementsByClassName("btn-start");
+
+// const start = document.querySelector(".btn-start");
+// console.log(startButton);
+// console.log(start);
+
+// const reset = document.querySelector("#reset");
+// console.log(reset);
+
+const buttons = document.querySelectorAll(".btn");
+
+console.log(buttons);
